cf41cc6d8c987b4cda174803eecdc7e81cd08316
kush <kush.baldha@gmail.com>
2016-10-22 16:10:01 -0400

    Creating the Parent Player class with movement and positions and speeds and stuff

---
+++ src/testing/Player.java
@@ -0,0 +1,59 @@
+package testing;
+
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+
+
+public class Player 
+{
+	int x;
+	int y;
+	int id;
+	double speedScalar;
+	int step = display something/ number;
+	int life = 3;
+	Rectangle hitbox;
+	BufferedImage image;
+	
+	public Player(int x, int y,int id, BufferedImage image)
+	{
+		this.x = x;
+		this.y = y;
+		this.id = id;
+		hitbox = new Rectangle(image.getWidth(),image.getHeight());
+		this.image = image;
+	}
+	public void setX(int x)
+	{
+		this.x = x;
+		hitbox.setLocation(x, y);
+	}
+	public void setY(int y)
+	{
+		this.y = y;
+		hitbox.setLocation(x, y);
+	}
+	
+	public void moveLeft()
+	{
+		setX(this.x - (int)speedScalar*step);
+	}
+	public void moveUp()
+	{
+		setY(this.y + (int)speedScalar*step);
+	}
+	public void moveRight()
+	{
+		setX(this.x + (int)speedScalar*step);
+		
+	}
+	public void moveDown()
+	{
+		setY(this.y - (int)speedScalar*step);
+	}
+	public double setSpeed(double speed)
+	{
+		this.speedScalar = speed;
+	}
+	
+}
