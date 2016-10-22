054b02f7cc6d13c708ff9778bde50e4824f1e4c3
kushbaldha <kush.baldha@gmail.com>
2016-10-22 17:59:23 -0400

    trying the background

--- src/testing/GamePanel.java
+++ src/testing/GamePanel.java
@@ -1,21 +1,37 @@
 	package testing;
 
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
-public class GamePanel 
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel
 {
 
 	Player player1;
 	Player player2;
-	BufferedImage background;
-	public GamePanel (BufferedImage image, BufferedImage image2,BufferedImage background)
+	public GamePanel ()
 	{
-		player1 = new RegPlayer(100,50,1,image);
-		player2 = new RegPlayer(400,50,2,image2);
-		this.background = background
 	}
 	
 	public void load()
+	{
+		//player1 = new RegPlayer(100,50,1,image);
+		//player2 = new RegPlayer(400,50,2,image2);
+		 BufferedImage backgroundImage = null;
+		 try {
+		     backgroundImage = ImageIO.read(new File("background.jpg"));
+		 } catch (IOException e) {
+		 }
+	}
+	public void update()
+	{
+		
+	}
+	public void paintComponent(Graphics G)
+	public void draw()
 	{
 		
 	}
--- src/testing/RegPlayer.java
+++ src/testing/RegPlayer.java
@@ -7,7 +7,6 @@ public class RegPlayer extends Player
 	
 	public RegPlayer(int x, int y,int id, BufferedImage image)
 	{
-		super(x,y,id,image);
-		
+		super(x,y,id,image); 
 	}
 }
