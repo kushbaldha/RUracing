4fe6f8984de4209c0b0d37904629f6e1a306979d
kushbaldha <kush.baldha@gmail.com>
2016-10-22 23:55:05 -0400

    nirav sucks

--- src/testing/GamePanel.java
+++ src/testing/GamePanel.java
@@ -17,9 +17,13 @@ public class GamePanel extends JPanel
 	Player player1;
 	Player player2;
 	BufferedImage backgroundImage = null;
-	int backgroundStep = 5;
-	ArrayList<Terrain> boulders;
-	File f;
+	BufferedImage brownBlock = null;
+	BufferedImage whiteBlock = null;
+	BufferedImage grayBlock = null;
+	int step = 5;
+	ArrayList<Terrain> boulders = new ArrayList<Terrain>();
+
+	Terrain tempTerrain;
 	public GamePanel()
 	{
 	setOpaque(true);	
@@ -29,9 +33,8 @@ public class GamePanel extends JPanel
 	{
 		//player1 = new RegPlayer(100,50,1,image);
 		//player2 = new RegPlayer(400,50,2,image2);
-		//boulders = new ArrayList<Terrain>();
+		
 		try{
-		      //f = new File("Background2.jpg"); //image file path
 		      backgroundImage = new BufferedImage(500,5000, BufferedImage.TYPE_INT_ARGB);
 		      backgroundImage = ImageIO.read(getClass().getResource("/images/Background.jpg"));
 
@@ -39,24 +42,90 @@ public class GamePanel extends JPanel
 		    }catch(IOException e){
 		      System.out.println("Error: "+e);
 		    }
-		}
+		
+		
+		try{
+		      brownBlock = new BufferedImage(500,5000, BufferedImage.TYPE_INT_ARGB);
+		      brownBlock = ImageIO.read(getClass().getResource("/images/Brown_block.jpg"));
+
+		      System.out.println("Reading complete.");
+		    }catch(IOException e){
+		      System.out.println("Error: "+e);
+		    }
+		
+		
+		try{
+		      whiteBlock = new BufferedImage(500,5000, BufferedImage.TYPE_INT_ARGB);
+		      whiteBlock = ImageIO.read(getClass().getResource("/images/White_Block.jpg"));
+
+		      System.out.println("Reading complete.");
+		    }catch(IOException e){
+		      System.out.println("Error: "+e);
+		    }
+		
+		
+		try{
+		      grayBlock = new BufferedImage(500,5000, BufferedImage.TYPE_INT_ARGB);
+		      grayBlock = ImageIO.read(getClass().getResource("/images/Gray_Block.jpg"));
+
+		      System.out.println("Reading complete.");
+		    }catch(IOException e){
+		      System.out.println("Error: "+e);
+		    }
+		
+		
+		    }
 	public void update()
 	{
 		
-		/*int rand = (int) Math.random()*20; //50% chance
-		if(rand>=19)
+		int rand = (int) (Math.random()*2); //% chance
+		if(rand>=1)
 		{
-			boulders.add(new Terrain());
-		}*/
+			int randomBlock =  (int) (Math.random()*3);
+			if(randomBlock == 0)
+				tempTerrain = new Terrain(brownBlock);
+				//boulders.add(new Terrain(brownBlock));
+			else if(randomBlock == 1)
+				tempTerrain = new Terrain(grayBlock);
+
+				//boulders.add(new Terrain(grayBlock));
+			else if(randomBlock == 2)
+				tempTerrain = new Terrain(whiteBlock);
+
+				//boulders.add(new Terrain(whiteBlock));
+			boulders.add(tempTerrain);
+			tempTerrain = null;
+			}
+		
+		for(int i = 0; i<boulders.size();i++)
+		{
+			tempTerrain = boulders.get(i);
+			if(tempTerrain.getY()>1010)
+			{
+				boulders.remove(i);
+			}
+		}
 	}
 	
 	public void paintComponent(Graphics g)
 	{
 		super.paintComponent(g);
-		g.setColor(Color.BLACK);
-		g.drawImage(backgroundImage,0,-4000+backgroundStep,this); 
-		g.fillRect(25,-3000+backgroundStep, 500, 100);
-		backgroundStep+=5;
+		
+		
+		g.drawImage(backgroundImage,0,-4000+step,this); 
+		
+		for(int i = 0;i<boulders.size();i++)
+		{
+			tempTerrain = boulders.get(i);
+			tempTerrain.setY(tempTerrain.getY()+5);
+			g.drawImage(tempTerrain.getImage(), tempTerrain.getX(), tempTerrain.getY(), this);
+		}
+		
+		
+		
+		step+=5;
+		if(step == 3000)
+			step = 1005;
 		//player1.draw(g,this);
 	}
 	
--- src/testing/Terrain.java
+++ src/testing/Terrain.java
@@ -1,19 +1,41 @@
 package testing;
 
+import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
 public class Terrain 
 {
 	int x;
 	int y;
-	
-	public Terrain()
-	{
+	BufferedImage image;	
+
+
+
+	public Terrain(BufferedImage image) {
 		x = (int) (Math.random()*500);
 		y = (int) (Math.random()*-100);
+		this.image = image;
 	}
-	
-	
+
+
+
+
+	public int getX()
+	{
+		return x;
+	}
+	public int getY()
+	{
+		return y;
+	}
+	public BufferedImage getImage()
+	{
+		return image;
+	}
+	public void setY(int y)
+	{
+		this.y = y;
+	}
 
 }
 
